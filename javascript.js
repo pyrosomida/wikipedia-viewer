57d9ad19e084f53a7f2288c18d40732f894ec68b
Thalia Silver <pyrosomida@gmail.com>
2016-08-05 18:49:28 +0100

    sorted out order of script links

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  console.log("doc ready");
   $('#search').on('click', function() {
     var searchTerm = $('#search_term').val();
     $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&format=json&redirects=resolve&search='+searchTerm+'&limit=15&callback=?', function(json) {
@@ -8,7 +9,7 @@ $(document).ready(function() {
       var links = json[3];
       var Entry = function () {};
       var EntryArr = [];
-      
+
       for (var i=0; i<15; i++) {
         var temp = new Entry();
         temp.title = titles[i];
@@ -19,7 +20,7 @@ $(document).ready(function() {
       for (var j=0; j<EntryArr.length; j++) {
           var temp2 = EntryArr[j];
       }
-      
+
       var html ='<br/>';
       EntryArr.forEach(function(val) {
         var keys = Object.keys(val);
